49d71bdf5a775a3e4764f3ee5e09a79063b8b099
Quentin Blampey <quentin.blampey@gmail.com>
2019-02-27 14:31:57 +0100

    works

--- src/components/Student/Chat/Loading.js
+++ src/components/Student/Chat/Loading.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { css } from '@emotion/core';
 import { BeatLoader } from 'react-spinners';
-import PropTypes from 'prop-types';
 
 const override = css`
     display: block;
@@ -10,29 +9,25 @@ const override = css`
 `;
 
 class Loading extends React.Component {
-    propTypes = {
-      loading: PropTypes.boolean.isRequired,
-    };
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
-    constructor(props) {
-      super(props);
-      this.state = {};
-    }
-
-    render() {
-      const { loading } = this.props;
-      return (
-        <div className="sweet-loading">
-          <BeatLoader
-            css={override}
-            sizeUnit="px"
-            size={12}
-            color="#123abc"
-            loading={loading}
-          />
-        </div>
-      );
-    }
+  render() {
+    const { loading } = this.props;
+    return (
+      <div className="sweet-loading">
+        <BeatLoader
+          css={override}
+          sizeUnit="px"
+          size={12}
+          color="#123abc"
+          loading={loading}
+        />
+      </div>
+    );
+  }
 }
 
 export default Loading;

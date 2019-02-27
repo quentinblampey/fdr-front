a4ae9d109e3934aa38d83d5c3692a2a2c9539676
MariusHull <marius.hullin@gmail.com>
2019-02-27 15:39:13 +0100

    fix CSS

--- src/components/Teacher/ComputeStats.js
+++ src/components/Teacher/ComputeStats.js
@@ -1,84 +1,82 @@
-function updateMotivation(user) {
+function updateMotivation() {
   return 0;
 }
 
 function updateFidelity(user) {
   if (
-    user.numberChats === undefined ||
-    user.numberChats === null ||
-    user.numberChats.length === 0
+    user.numberChats === undefined
+        || user.numberChats === null
+        || user.numberChats.length === 0
   ) {
     return 0;
-  } else {
-    var score = 0;
-    var d = new Date();
-    var today = [d.getFullYear(), d.getMonth() + 1, d.getDate()];
-
-    var lastChat = user.numberChats[user.numberChats.length - 1];
-    var lastEval = [
-      lastChat.split("T")[0].split("-")[0],
-      lastChat.split("T")[0].split("-")[1],
-      lastChat.split("T")[0].split("-")[2]
-    ];
-
-    var dateRegistration = [
-      user.registration.split("T")[0].split("-")[0],
-      user.registration.split("T")[0].split("-")[1],
-      user.registration.split("T")[0].split("-")[2]
-    ];
-
-    if (today[1] - lastEval[1] >= 1 || today[0] - lastEval[0] >= 1) {
-      score += 1;
-    } else if (today[2] - lastEval[2] > 21) {
-      score += 2;
-    } else if (today[2] - lastEval[2] > 14) {
-      score += 3;
-    } else if (today[2] - lastEval[2] > 7) {
-      score += 4;
-    } else if (today[2] - lastEval[2] < 7) {
-      score += 5;
-    }
-
-    var nbchats = user.numberChats.length;
-    var nbMois =
-      (today[0] - dateRegistration[0]) * 12 + today[1] - dateRegistration[1];
-
-    if (nbMois === 0 && nbchats === 1) {
-      score += 2;
-    } else if (nbMois === 0 && nbchats === 2) {
-      score += 4;
-    } else if (nbchats > 2 * nbMois) {
-      score += 5;
-    } else if (nbchats > nbMois) {
-      score += 3;
-    } else if (nbchats < nbMois) {
-      score += 1;
-    }
-
-    return score;
   }
+  let score = 0;
+  const d = new Date();
+  const today = [d.getFullYear(), d.getMonth() + 1, d.getDate()];
+
+  const lastChat = user.numberChats[user.numberChats.length - 1];
+  const lastEval = [
+    lastChat.split('T')[0].split('-')[0],
+    lastChat.split('T')[0].split('-')[1],
+    lastChat.split('T')[0].split('-')[2],
+  ];
+
+  const dateRegistration = [
+    user.registration.split('T')[0].split('-')[0],
+    user.registration.split('T')[0].split('-')[1],
+    user.registration.split('T')[0].split('-')[2],
+  ];
+
+  if (today[1] - lastEval[1] >= 1 || today[0] - lastEval[0] >= 1) {
+    score += 1;
+  } else if (today[2] - lastEval[2] > 21) {
+    score += 2;
+  } else if (today[2] - lastEval[2] > 14) {
+    score += 3;
+  } else if (today[2] - lastEval[2] > 7) {
+    score += 4;
+  } else if (today[2] - lastEval[2] < 7) {
+    score += 5;
+  }
+
+  const nbchats = user.numberChats.length;
+  const nbMois = (today[0] - dateRegistration[0]) * 12 + today[1] - dateRegistration[1];
+
+  if (nbMois === 0 && nbchats === 1) {
+    score += 2;
+  } else if (nbMois === 0 && nbchats === 2) {
+    score += 4;
+  } else if (nbchats > 2 * nbMois) {
+    score += 5;
+  } else if (nbchats > nbMois) {
+    score += 3;
+  } else if (nbchats < nbMois) {
+    score += 1;
+  }
+
+  return score;
 }
 
-function updateLifestyle(user) {
+function updateLifestyle() {
   return 0;
 }
 
-function updateIntegration(user) {
+function updateIntegration() {
   return 0;
 }
 
-function updateNoOrientation(user) {
+function updateNoOrientation() {
   return 0;
 }
 
 export default function computeStats(user) {
-  var tabScores = [];
-  var count = 0;
-  Object.keys(user.score).forEach(score => {
+  const tabScores = [];
+  let count = 0;
+  Object.keys(user.score).forEach((score) => {
     if (score >= 0) {
       tabScores.push(score);
     } else {
-      var newScore;
+      let newScore;
       switch (count) {
         case 0:
           newScore = updateMotivation(user);
@@ -105,7 +103,7 @@ export default function computeStats(user) {
       }
       tabScores.push(newScore);
     }
-    count = count + 1;
+    count += 1;
   });
 
   return tabScores;

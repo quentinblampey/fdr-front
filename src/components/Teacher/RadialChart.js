3bd919ce13fc13076aebcf68fa9e605d85ab8795
MariusHull <marius.hullin@gmail.com>
2019-02-27 15:40:49 +0100

    saved files

--- src/components/Teacher/RadialChart.js
+++ src/components/Teacher/RadialChart.js
@@ -1,41 +1,43 @@
 class RadialChart extends React.Component {
-      
-    constructor(props) {
-      super(props);
+  constructor(props) {
+    super(props);
 
-      this.state = {
-        options: {
-          labels: ['Apples', 'Oranges', 'Bananas', 'Berries'],
-          radialBar: {
-            dataLabels: {
-              name: {
-                fontSize: '22px',
+    this.state = {
+      options: {
+        labels: ['Apples', 'Oranges', 'Bananas', 'Berries'],
+        radialBar: {
+          dataLabels: {
+            name: {
+              fontSize: '22px',
+            },
+            value: {
+              fontSize: '16px',
+            },
+            total: {
+              show: true,
+              label: 'Total',
+              formatter(w) {
+                // By default this function returns the average of all series. The below is just an example to show the use of custom formatter function
+                return 249;
               },
-              value: {
-                fontSize: '16px',
-              },
-              total: {
-                show: true,
-                label: 'Total',
-                formatter: function (w) {
-                  // By default this function returns the average of all series. The below is just an example to show the use of custom formatter function
-                  return 249
-                }
-              }
-            }
-          }
+            },
+          },
         },
-        series: [44, 55, 67, 83],
-      }
-    }
+      },
+      series: [44, 55, 67, 83],
+    };
+  }
 
-    render() {
-      return (
-        
-
-        <div id="chart">
-          <ReactApexChart options={this.state.options} series={this.state.series} type="radialBar" height="350" />
-        </div>
-      )
-    }
+  render() {
+    return (
+      <div id="chart">
+        <ReactApexChart
+          options={this.state.options}
+          series={this.state.series}
+          type="radialBar"
+          height="350"
+        />
+      </div>
+    );
+  }
 }

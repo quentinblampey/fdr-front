3a33ab46d6de84797e03dc267cad0df9b86a7701
Quentin Blampey <quentin.blampey@gmail.com>
2019-02-28 10:15:20 +0100

    suppress loading warning

--- src/components/Student/Chat/Loading.js
+++ src/components/Student/Chat/Loading.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import { css } from '@emotion/core';
+// import { css } from '@emotion/core';
 import { BeatLoader } from 'react-spinners';
 
+/*
 const override = css`
     display: block;
     margin: 0 auto;
     border-color: red;
 `;
+*/
 
 class Loading extends React.Component {
   constructor(props) {
@@ -18,13 +20,7 @@ class Loading extends React.Component {
     const { loading } = this.props;
     return (
       <div className="sweet-loading">
-        <BeatLoader
-          css={override}
-          sizeUnit="px"
-          size={12}
-          color="#123abc"
-          loading={loading}
-        />
+        <BeatLoader sizeUnit="px" size={12} color="#123abc" loading={loading} />
       </div>
     );
   }

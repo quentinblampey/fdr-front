616ba231c1de22db56bc25f946e1ee42fa02d393
Quentin Blampey <quentin.blampey@gmail.com>
2019-03-04 10:25:05 +0100

    add sncf

--- src/components/Student/Begin.js
+++ src/components/Student/Begin.js
@@ -1,48 +1,50 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
-import FooterStop from './FooterStop'
-import url from '../../config'
+import FooterStop from './FooterStop';
+import url from '../../config';
+import './Begin.scss';
 
 class Begin extends Component {
-
   constructor(props) {
     super(props);
     this.state = {
-      user:''
+      user: '',
     };
   }
 
   componentDidMount() {
-
-    axios.get(url+`/api/users/getid/${this.props.match.params.id}`)
-      .then(res => {
-        this.setState({ user:res.data });
-      });
-
+    axios.get(`${url}/api/users/getid/${this.props.match.params.id}`).then((res) => {
+      this.setState({ user: res.data });
+    });
   }
 
   render() {
     return (
-      <div className="container">
-        <div className="panel panel-default">
-            <div className="panel-body">
-                <h1>Bienvenue dans l'application d'aide à la réussite !</h1>
-                <h1>Bonjour, {this.state.user.pseudo}</h1>
-                <h3>Vous pouvez commencer à chatter avec le bot pour vous évaluer : </h3>
-
-                <Link to={`/chat/${this.props.match.params.id}`}>
-                    <button type="submit" className="btn btn-success">Commencer la discussion !</button>
-                </Link>
-            </div>
+      <div className="component">
+        <div className="shape">
+          <div className="left">
+            <div />
+          </div>
+          <Link to={`/chat/${this.props.match.params.id}`}>
+            <button type="submit" className="content">
+              <p>
+                <span>“ </span>
+                                MON COACH CHATBOT
+                <span> ”</span>
+              </p>
+            </button>
+          </Link>
         </div>
-        <FooterStop/>
-
+        <Link to="">
+          <button type="submit" className="help">
+            <p>DEMANDER DE L'AIDE</p>
+          </button>
+        </Link>
+        <FooterStop />
       </div>
     );
   }
 }
-
-
 
 export default Begin;
--- src/components/Student/FooterStop.js
+++ src/components/Student/FooterStop.js
@@ -1,19 +1,23 @@
 import React, { Component } from 'react';
-//import axios from 'axios';
+// import axios from 'axios';
 import { Link } from 'react-router-dom';
 
 class FooterStop extends Component {
   render() {
     return (
-        <div>
-            <nav className="navbar fixed-bottom navbar-light bg-light">
-                <Link to={`/exit/`}>
-                    <button type="submit" className="btn btn-danger">Arrêter l'évaluation</button>
-                </Link>
-            </nav>
-        </div>
+      <div>
+        <nav
+          className="navbar fixed-bottom navbar-light bg-light"
+          style={{ display: 'flex', justifyContent: 'center' }}
+        >
+          <Link to="/exit/">
+            <button type="submit" className="btn btn-danger">
+                            Arrêter l'évaluation
+            </button>
+          </Link>
+        </nav>
+      </div>
     );
   }
 }
 export default FooterStop;
-

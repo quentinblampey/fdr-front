6251dc12264c2a023d8d2e1fd9911095e83431b4
Quentin Blampey <quentin.blampey@gmail.com>
2019-03-06 17:59:07 +0100

    warings

--- src/components/Teacher/VueEnseignant.js
+++ src/components/Teacher/VueEnseignant.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 // import ReactDOM from 'react-dom';
-import { Link } from 'react-router-dom';
 import axios from 'axios';
 import url from '../../config';
 import variables from '../../globalSCSS/color.scss';
@@ -14,20 +13,19 @@ class VueEnseignant extends Component {
     this.state = {
       pseudo: '',
       pseudos: [],
-      filter:'pseudo',
+      filter: 'pseudo',
       profils: ['Employés', 'Sportifs', 'Handicapés', 'Artistes'],
       profilsName: ['employe', 'athlete', 'disabled', 'artist'],
       proportions: [0, 0, 0, 0],
       colors: [variables.graph1, variables.graph2, variables.graph3, variables.graph4],
-      updateFilter:this.updateFilter.bind(this), 
-  }
+      updateFilter: this.updateFilter.bind(this),
     };
+  }
 
   componentDidMount() {
-    axios.post(`${url}/api/stats/profils`, { profils: this.state.profilsName })
-      .then((res) => {
-        this.setState({ proportions: res.data.proportions });
-      });
+    axios.post(`${url}/api/stats/profils`, { profils: this.state.profilsName }).then((res) => {
+      this.setState({ proportions: res.data.proportions });
+    });
     axios.get(`${url}/api/users/`).then((res) => {
       this.setState({ pseudo: '', pseudos: res.data });
     });
@@ -35,7 +33,7 @@ class VueEnseignant extends Component {
 
   updateFilter(filter) {
     console.log(filter);
-    this.setState({filter : filter});
+    this.setState({ filter });
     console.log(this.state.filter);
   }
 
@@ -46,19 +44,33 @@ class VueEnseignant extends Component {
         <div className="row">
           <div className="col-9">
             <div className="row text-center">
-              <button type="button" className="btn btn-primary" style={{width:'100%'}} onClick={this.updateFilter.bind(this, 'pseudo')}>Afficher la liste complète</button>
+              <button
+                type="button"
+                className="btn btn-primary"
+                style={{ width: '100%' }}
+                onClick={this.updateFilter.bind(this, 'pseudo')}
+              >
+                                Afficher la liste complète
+              </button>
             </div>
             <div className="row">
               <div className="col-6">
-                <RadialChart updateFilter={this.state.updateFilter} parent={this} profils= {this.state.profils} profilsName = {this.state.profilsName} proportions={this.state.proportions} colors={this.state.colors}></RadialChart>
+                <RadialChart
+                  updateFilter={this.state.updateFilter}
+                  parent={this}
+                  profils={this.state.profils}
+                  profilsName={this.state.profilsName}
+                  proportions={this.state.proportions}
+                  colors={this.state.colors}
+                />
               </div>
               <div className="col-6">
-                <Repartition updateFilter={this.state.updateFilter}></Repartition>
+                <Repartition updateFilter={this.state.updateFilter} />
               </div>
             </div>
           </div>
           <div className="col-3">
-            <Filtered filter={this.state.filter} style={{maxWidth:'80%'}}></Filtered>
+            <Filtered filter={this.state.filter} style={{ maxWidth: '80%' }} />
           </div>
         </div>
       </div>

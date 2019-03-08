8e74cbe7fc71820cfa5d484c77bfdda0da950447
Nicolas GABRION <nico.gabrion@gmail.com>
2019-03-08 09:53:51 +0100

    -

---
+++ src/components/Teacher/Info.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import './FicheCourte.scss';
+import { Link } from 'react-router-dom';
+import ReactTooltip from 'react-tooltip';
+
+class Info extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        texts : {
+            profils : 'Dans cette partie du Dashboard, <br/>vous pouvez clicker sur chacune <br/>des radial bars afin que la liste des <br/>élèves proposés soit filtrée pour <br/>ce profil.',
+            repartition : 'Dans cette partie du Dashboard, <br/>vous pouvez clicker sur chacun <br/>des élements afin que la liste des <br/>élèves proposés soit ordonnée en <br/>fonction de ce critère.'
+        }
+    };
+  }
+
+  componentDidMount() {
+  }
+
+  componentDidUpdate(prevProps){}
+
+    render() {
+      return (
+        <div>
+            <ReactTooltip multiline={true}></ReactTooltip>
+            <button multiline={true} type="button" className="btn btn-primary rounded-circle" data-tip={this.state.texts[this.props.part]}>
+                &nbsp;i&nbsp;
+            </button>
+        </div>
+        
+      );
+    }
+}
+
+export default Info;
